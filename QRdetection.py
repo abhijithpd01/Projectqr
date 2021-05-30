b3d91ff1ba21170313500b3873dee45aaffd837d
Abhijith <68745732+abhijithpd01@users.noreply.github.com>
2021-05-31 01:28:27 +0530

    Update QRdetection.py

--- QRdetection.py
+++ QRdetection.py
@@ -7,26 +7,22 @@ cap.set(4, 480)
 
 while True:
     success, img = cap.read()
-    for barcode in decode(img):
-        print(barcode.data)
-        myData = barcode.data.decode('utf-8')
-        print(myData)
-        pts = np.array([barcode.polygon], np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(img, [pts], True, (255, 0, 255), 5)
     cv2.imshow('Result', img)
-    cv2.waitKey(1)
-if(pd==26831)
-   print("location 1")
-    break
-if(pd==26832)
-   print("location 2")
-    break
-if(pd==26833)
-   print("location 3")
-    break
-if(pd==26834)
-   print("location 4")
-    break   
+    cv2.waitKey(10)
+    code=decode(img)
+
+
+    for qrCode in code:
+        myData = qrCode.data.decode('utf-8')
+        data = myData.split(",")
+        print("zip:" + data[0])
+        print("House:" + data[1])
+        print("Place:" + data[2])
+        pin='679531'
+        if data[0] == pin:
+            print("Place the Product in Box A")
+        else:
+            print("Place the Product in Box B")
+
 
     
